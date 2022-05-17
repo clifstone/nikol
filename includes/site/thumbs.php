391bf1502393d3d9d0d8e5d387dfdb274c5d8e9a
clifstone <47040820+clifstone@users.noreply.github.com>
2022-05-16 22:27:08 -0500

    lot of aspect ratios. can't expect user to adjust manually. adjusted includes/site/thumbs.php, and addionally, the gallery thumb in includes/shortcodes/supergallery.php so that images appear less chunky

--- includes/site/thumbs.php
+++ includes/site/thumbs.php
@@ -2,14 +2,14 @@
 
 function wpdocs_theme_setup() {
     add_theme_support( 'post-thumbnails' );
-    add_image_size( 'exex-large', 2048 );
-    add_image_size( 'ex-large', 1920 );
-    add_image_size( 'large', 1536 );
-    add_image_size( 'medium', 1024 );
-    add_image_size( 'small', 600 );
-    add_image_size( 'extra-small', 338 );
-    add_image_size( 'tiny', 180 );
-    set_post_thumbnail_size( 600, array( 'top', 'left')  );
+    add_image_size( 'exex-large', 2048, 9999, false );
+    add_image_size( 'ex-large', 1920, 9999, false );
+    add_image_size( 'large', 1536, 9999, false );
+    add_image_size( 'medium', 1024, 9999, false );
+    add_image_size( 'small', 600, 9999, false );
+    add_image_size( 'extra-small', 338, 9999, false );
+    add_image_size( 'tiny', 180, 9999, false );
+    set_post_thumbnail_size( 600, 600, array( 'top', 'left')  );
 }
 add_action( 'after_setup_theme', 'wpdocs_theme_setup' );
 
